d3285aa04773f847f0d7d51624bb92cdbb53ac3e
HrytsenkoEN <ivgenius@gmail.com>
2023-02-01 23:53:40 +0200

    task-05

--- js/task-04.js
+++ js/task-04.js
@@ -17,7 +17,7 @@
 
 const decrementBtn = document.querySelector('[data-action="decrement"]');
 const incrementBtn = document.querySelector('[data-action="increment"]');
-const valueEl = document.querySelector('[id="value"]');
+const valueEl = document.querySelector('#value');
 
 let counterValue = 0;
 
