e0c28551038e1f6220525d7a58c3cf6d93d0d990
HrytsenkoEN <ivgenius@gmail.com>
2023-02-02 19:32:28 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// Напиши скрипт, який змінює кольори фону елемента
+//   < body > через інлайн - стиль по кліку на
+// button.change - color і виводить значення кольору 
+// в span.color.
+
+const nameColor = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+const body = document.querySelector('body');
+
+changeColorBtn.addEventListener('click', () => {
+  const bgColor = getRandomHexColor();
+  body.style.backgroundColor = bgColor;
+  nameColor.textContent = bgColor;
+})
