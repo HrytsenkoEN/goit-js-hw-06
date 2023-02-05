a8cabecdf6bcf28092076716b053d94440b7cd5b
HrytsenkoEN <ivgenius@gmail.com>
2023-02-05 23:03:29 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,15 +1,25 @@
 
 
-const counter = parent => { return parent.childElementCount };
-const message = elements => {
-    elements.forEach(element => {
-        const firstEl = element.firstElementChild;
-        const secondElem = firstEl.nextElementSibling
-        console.log(`Category: ${firstEl.textContent}`);
-        console.log(`Elements: ${counter(secondElem)}`);
-    });
-};
-const categories = document.querySelector('#categories');
-const subCategories = categories.querySelectorAll('.item');
-console.log(`Number of categories: ${counter(categories)}`);
-message(subCategories);
+// const counter = parent => { return parent.childElementCount };
+// const message = elements => {
+//     elements.forEach(element => {
+//         const firstEl = element.firstElementChild;
+//         const secondElem = firstEl.nextElementSibling
+//         console.log(`Category: ${firstEl.textContent}`);
+//         console.log(`Elements: ${counter(secondElem)}`);
+//     });
+// };
+// const categories = document.querySelector('#categories');
+// const subCategories = categories.querySelectorAll('.item');
+// console.log(`Number of categories: ${counter(categories)}`);
+// message(subCategories);
+
+const categories = document.querySelectorAll('.item');
+
+console.log('Number of categories', categories.length);
+console.log('');
+categories.forEach((element) => {
+    console.log('Category:', element.firstElementChild.textContent);
+    console.log('Elements:', element.lastElementChild.children.length);
+    console.log('');
+})
